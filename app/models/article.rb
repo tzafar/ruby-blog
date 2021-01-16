2a71c02615b327c3f5c33164e5196210f9d19c10
Toseef Zafar <mtouseef.zafar@gmail.com>
2021-01-16 11:55:22 +0000

    added db migration for users

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   include Visible
   has_many :comments
+  belongs_to :user
   validates :title, presence: true, :allow_blank => false, :allow_nil => false, :length => {minimum: 3, maximum: 100}
   validates :body, presence: true, :allow_blank => false, :allow_nil => false, :length => {minimum: 10, maximum: 200}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_081119) do
+ActiveRecord::Schema.define(version: 2021_01_16_114612) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_01_16_081119) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -29,5 +31,14 @@ ActiveRecord::Schema.define(version: 2021_01_16_081119) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "fullname"
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
 end
