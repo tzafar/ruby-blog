975efb3dd228d2827a90cb05d70a16ab5bd9cb0d
Toseef Zafar <mtouseef.zafar@gmail.com>
2021-01-20 23:10:47 +0000

    we only want to show that notice for only sign in request, dont want to keep for next http request.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
-      flash[:notice] = "The credentials are not correct"
+      flash.now[:notice] = "The credentials are not correct"
       render :login
     end
   end
