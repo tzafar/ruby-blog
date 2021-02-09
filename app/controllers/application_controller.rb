2cc390cd05299e57fa51b53f844d45f3d8c06ca0
Toseef Zafar <mtouseef.zafar@gmail.com>
2021-02-09 14:20:02 +0000

    a user should only be able to create new article when logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in first"
+      redirect_to login_path
+    end
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :require_user, except: [:show, :index]
+
   def index
     @articles = Article.all
   end
