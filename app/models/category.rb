729a7de2b34cac80cdb16b327cbaebbf97044cef
Toseef Zafar <mtouseef.zafar@gmail.com>
2021-02-16 22:35:45 +0000

    added tests for model Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  validates :title,
+            presence: true,
+            length: { minimum: 3, maximum: 12 },
+            format: { with: /\A[a-zA-Z]+\z/, message: 'Category title should only cotains letters' }
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "category should not have empty title" do
+    category = Category.new({ title: '' })
+    assert_not category.valid?
+  end
+
+  test 'category should not have less than 3 characters' do
+    category = Category.new({ title: 'st' })
+    assert_not category.valid?
+  end
+
+  test 'category should not have more than 12 characters' do
+    category = Category.new({ title: 'SportsGalaSpecial' })
+    assert_not category.valid?
+  end
+
+  test 'category should have the right length of characters' do
+    category = Category.new({ title: 'Sports' })
+    assert category.valid?
+  end
+
+  test 'category should only contains letters' do
+    category = Category.new({ title: '123456' })
+    assert_not category.valid?
+  end
 end
